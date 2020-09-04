fb134557a22ba51ae6cfca30db2169ce2e580b23
KuruzuKasutijo <38448829+KuruzuKasutijo@users.noreply.github.com>
2020-09-03 23:39:17 -0500

    Add files via upload

---
+++ J22s/menu-verticalMenus.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+<!--
+function mmLoadMenus() {
+  if (window.mm_menu_0716013432_0) return;
+                                                                                                                      window.mm_menu_0716013432_0 = new Menu("root",330,85,"Verdana",23,"#FFFFFF","#FFFFFF","#181A18","#00A7D1","left","middle",10,0,800,-5,7,true,true,true,0,false,false);
+  mm_menu_0716013432_0.addMenuItem("Menu","window.open('../index.html', '_self');");
+  mm_menu_0716013432_0.addMenuItem("Buscar","window.open('../bU2C4d0/buscar.html', '_self');");
+  mm_menu_0716013432_0.addMenuItem("Subido","window.open('../Nu3V0/index.html', '_self');");
+  mm_menu_0716013432_0.addMenuItem("Canales","window.open('../C4N4l32/index.html', '_self');");
+  mm_menu_0716013432_0.addMenuItem("Acerca&nbsp;de...","window.open('../4c3Rc4D3/index.html', '_self');");
+   mm_menu_0716013432_0.fontWeight="bold";
+   mm_menu_0716013432_0.hideOnMouseOut=true;
+   mm_menu_0716013432_0.bgColor='#333333';
+   mm_menu_0716013432_0.menuBorder=5;
+   mm_menu_0716013432_0.menuLiteBgColor='#333333';
+   mm_menu_0716013432_0.menuBorderBgColor='#181A18';
+
+mm_menu_0716013432_0.writeMenus();
+} // mmLoadMenus()
+//-->
