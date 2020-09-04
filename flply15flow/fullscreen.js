e01f39ad5f07edd9bc39c5ce88e185f6544d1310
KuruzuKasutijo <38448829+KuruzuKasutijo@users.noreply.github.com>
2020-09-03 23:45:39 -0500

    Add files via upload

---
+++ flply15flow/fullscreen.js
@@ -0,0 +1,13 @@
+/*
+ * Default FlowPlayer fullscreen opener.
+ * http://flowplayer.sourceforge.net
+ */
+
+function flowPlayerOpenFullScreen(config) {
+if(window.screen.availWidth == 1280);
+}
+
+function flowPlayerExitFullScreen(config) {
+  self.close();
+}
+
