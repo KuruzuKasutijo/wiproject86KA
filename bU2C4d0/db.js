d4c8a7a7b1874d644a9dffc7a4c7084465b864be
KuruzuKasutijo <38448829+KuruzuKasutijo@users.noreply.github.com>
2020-09-03 23:49:51 -0500

    Add files via upload

---
+++ bU2C4d0/db.js
@@ -0,0 +1,32 @@
+/* XSearch version 5.2 - Database file */
+
+/* configuration settings */
+
+	searchname = 'buscar.html'
+	
+	usebannercode=true
+ButtonCode = "<button type='submit'>Buscar</button>"
+	function templateBody() {
+		document.write('<'+
+		'script language="Javascript">'+
+		'<'+'/'+'script'+'></head><body bgcolor="#ffffff" text="#FFFFFF" link="#FFFFFF" vlink="#FFFFFF" alink="#FFFFFF"><table border=0 width=800><tr><td>');
+	}
+
+	function templateEnd() {
+		document.write('</td></tr></table></font></center></body></html>');
+	}
+	function bannerCode() {
+	}	
+
+/* end configuration settings */
+
+/* database records */
+
+
+
+add("<a href='../C4N4l32/DDRIB021OT/2020/Entrandoenclasesonlin/Fangames.html'>ENTRANDO EN CLASES ONLINE DE ESTUDIANTES 4!</a>","Bromasclasesonline clasesonline didacribot bromasonlinecuarentena clasesonline","Hola, soy Didac Ribot. Hoy os traigo la parte 4 de esta sección, donde entro en clases online de estudiantes y realizo actuaciones cómicas para ver las reacciones de los profesores y alumnos. Espero que os guste!")
+add("<a href='../C4N4l32/DDRIB021OT/2020/Durmiendoenlastoallas/Fangames.html'>DURMIENDO EN LAS TOALLAS DE LA GENTE EN LA PLAYA!</a>","bromadurmiendoentoallasdelagente bromaenlaplaya didac ribot bromadidacribottoallasagenas","Hola, soy Didac Ribot. En el vídeo de hoy voy a dormir en las toallas de la gente en la playa. Veremos como reaccionan las personas con un desconocido que se pone a roncar gravemente en sus proximidades.  !Disfrutad del vídeo!")
+add("<a href='../C4N4l32/DDRIB021OT/2020/Llamadsincomodas/Fangames.html'>LLAMADAS INCÓMODAS con TELÉFONO GIGANTE [Cámara Oculta]</a>","Llamadasincomodascontelefonogigantebroma bromatelefonogigante didac ribot bromaocultatelefonogigante","Hola, soy Didac Ribot. En este vídeo voy a la playa con un teléfono demasiado grande y simulo tener conversaciones incómodas y divertidas con gente. La reacciones de las personas al ver un teléfono de este tamaño no tienen precio. Disfrutad del vídeo!")
+add("<a href='../C4N4l32/DDRIB021OT/2020/Camareroinfiltr/Fangames.html'>CAMARERO INFILTRADO EN TABERNA IRLANDESA!</a>","Bromacamareroinfiltrado didiacribotbromacamarero bromaenunataberna tabernairlandesabroma ","Hola, soy Didac Ribot. En este vídeo me hago pasar por camarero en una taberna irlandesa. Voy a crear situaciones incómodas y cómicas con los clientes para ver sus reacciones. Muchas gracias a Guinnes Tavern de Salou por permitirme grabar en sus instalaciones. Disfrutad del vídeo!")
+add("<a href='../C4N4l32/DDRIB021OT/2020/ElVigilanteplaya/Fangames.html'>EL VIGILANTE DE LA PLAYA! [Cámara Oculta]</a>","Bromaplayadidac Vigilanteenlaplayabroma bromadidacenlaplaya camaraocultabromaenlaplayadidacribot","Hola, soy Didac Ribot. En este vídeo me hago pasar por socorrista en la playa. Voy a inventarme normas y crear situaciones graciosas e incómodas con la gente. Disfrutad del vídeo!")
+add("<a href='../C4N4l32/E1FdL0b02BO/2020/Los5MejoresPSV/Fangames.html'>Los 5 Mejores Juegos del PS VITA I El Fedelobo</a>","5mejoresjuegosdepsvita juegospsvita Elfedelobo topjuegosdepsvita Mejores5juegosvita psvita","Los 5 Mejores Juegos del PS VITA I Fedelobo")
